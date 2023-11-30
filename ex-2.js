f35be6827cc6c2bec319d3bea1cf7a3b98f76aba
Waramet Hatjantuek <waramet.fl@gmail.com>
2023-11-30 14:32:40 +0700

    feat: add completed exercise

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,10 @@
 //Exercise 2
 let lightBulbStatus = "On";
-
+// lightBulbStatus = "Broken";
 // Start coding here.
+
+if (lightBulbStatus === "Broken") {
+  console.log("Light bulb is Broken.");
+} else {
+  console.log("Please choose the correct input (On/Off/Broken)");
+}
