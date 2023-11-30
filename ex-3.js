b04793d1a4156ff39db33029048c7ee9b4f38566
unknown <tb.poopp@gmail.com>
2023-11-30 16:04:03 +0700

    feat: add exercise conditional logic

--- ex-3.js
+++ ex-3.js
@@ -1,5 +1,11 @@
 //Exercise 3
 // Ternary Operator
 let lightBulbStatus = "Off";
+let message =
+  lightBulbStatus === "On"
+    ? "Light bulb is On."
+    : lightBulbStatus === "Off"
+    ? "Light bulb is Off"
+    : "Please choose the correct input (On/Off)";
 
-//Start coding here
+console.log(message);
